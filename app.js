c8b6829ff159c2a08543fdfbb365660808de44d9
xbaysal11 <baisalbek.daniiarov@gmail.com>
2020-10-09 23:18:08 +0600

    ADD auth route

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const config = require('config');
+const mongoose = require('mongoose');
 
 const app = express();
 
+app.use(express.json({ extended: true }));
+app.use('/api/auth', require('./routes/auth.routes'));
+
 const PORT = config.get('port') || 8080;
 
-app.listen(PORT, () => console.log(`PORT ${PORT}`));
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoURI'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        });
+        app.listen(PORT, () => console.log(`PORT ${PORT}`));
+    } catch (e) {
+        console.log('Server Error', e.message);
+        process.exit(1);
+    }
+}
 
+start();
---
+++ routes/auth.routes.js
@@ -0,0 +1,87 @@
+const { Router } = require('express');
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const router = Router();
+
+// /api/auth/register
+router.post(
+    '/register',
+    [
+        check('email', 'invalid email').isEmail(),
+        check('password', 'password min length is 6 symbols').isLength({
+            min: 6,
+        }),
+    ],
+    async (req, res) => {
+        try {
+            console.log('body: ', req.body);
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'invalid data for registration',
+                });
+            }
+
+            const { email, password } = req.body;
+            const condidate = await User.findOne({ email });
+            if (condidate) {
+                return res.status(400).json({ message: 'User is exist' });
+            }
+            const hashedPassword = await bcrypt.hash(password, 12);
+            const user = new User({ email, password: hashedPassword });
+            await user.save();
+            res.status(201).json({ message: 'User created' });
+        } catch (e) {
+            res.status(500).json({
+                errors: e.message,
+                message: 'Something is going wrong',
+            });
+        }
+    }
+);
+
+// /api/auth/login
+router.post(
+    '/login',
+    [
+        check('email', 'enter valid email').normalizeEmail().isEmail(),
+        check('password', 'enter password').exists(),
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'invalid data for login',
+                });
+            }
+
+            const { email, password } = req.body;
+            const user = await User.findOne({ email });
+            if (!user) {
+                return res.status(400).json({ message: 'User not exist' });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                return res.status(400).json({ message: 'invalid password' });
+            }
+
+            const token = jwt.sign(
+                { userId: user.id },
+                config.get('jwtSecretKey')
+            );
+
+            res.status(200).json({ token, userId: user.id });
+        } catch (e) {
+            res.status(500).json({ message: 'Something is going wrong' });
+        }
+    }
+);
+
+module.exports = router;
