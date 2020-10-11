2acda9e566aae7ae631201c272ff7916b4b25f44
xbaysal11 <baisalbek.daniiarov@gmail.com>
2020-10-11 23:54:23 +0600

    ADD heroku setup

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express, { Router } = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
-
+const router = Router();
 const app = express();
+
 mongoose.set('useFindAndModify', false);
 app.use(express.json({ extended: true }));
-
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/wallet', require('./routes/wallet.routes'));
 app.use('/api/income-category', require('./routes/incomeCategory.routes'));
@@ -13,6 +13,10 @@ app.use('/api/expense-category', require('./routes/expenseCategory.routes'));
 app.use('/api/income', require('./routes/income.routes'));
 app.use('/api/expense', require('./routes/expense.routes'));
 app.use('/api/transfer', require('./routes/transfer.routes'));
+
+router.get('/', (req,res)=>{
+    res.send('xWallet')
+});
 
 const PORT = process.env.PORT || config.get('port');
 
